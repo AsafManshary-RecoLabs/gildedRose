5b2789576952522416354eff89237106b1973a04
AsafManshary-RecoLabs <asafm@recolabs.ai>
2022-07-07 16:11:32 +0300

    gilded-rose: refactor and test gilded-rose
    Add refactoring using factory method and command desgin patterns and
    test it
    
    this is part of RND-XXX
    
    signed-of-by: shayn (shayn@recolabs.ai)

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -5,80 +5,11 @@ type Item struct {
 	SellIn, Quality int
 }
 
-func UpdateQuality(items []*Item) (err error) {
+func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-		err = UpdateQualityPerItem(items[i])
-		if err == nil {
-			return err
-		}
+		UpdateQualityPerItem(items[i])
 	}
-	return nil
 }
-func UpdateOther(item *Item) (err error) {
-	if item.Quality < 50 {
-		item.Quality = item.Quality + 1
-	}
-	item.SellIn = item.SellIn - 1
-	if item.Quality < 50 {
-		item.Quality = item.Quality + 1
-	}
-	if item.SellIn < 0 {
-
-	}
-	return err
-}
-func UpdateSulf(item *Item) (err error) {
-	return err
-}
-
-func UpdateBrie(item *Item) (err error) {
-	if item.Quality < 50 {
-		item.Quality = item.Quality + 1
-	}
-	item.SellIn = item.SellIn - 1
-	if item.Quality < 50 {
-		item.Quality = item.Quality + 1
-	}
-	if item.SellIn < 0 {
-
-	}
-	return err
-}
-func UpdateTafkal(item *Item) (err error) {
-	if item.Quality < 50 {
-		item.Quality = item.Quality + 1
-		if item.SellIn < 11 {
-			if item.Quality < 50 {
-				item.Quality = item.Quality + 1
-			}
-		}
-		if item.SellIn < 6 {
-			if item.Quality < 50 {
-				item.Quality = item.Quality + 1
-			}
-		}
-	}
-	item.SellIn = item.SellIn - 1
-	if item.Quality < 50 {
-		item.Quality = item.Quality + 1
-	}
-	if item.SellIn < 0 {
-		item.Quality = item.Quality - item.Quality
-	}
-	return err
-}
-func UpdateQualityPerItem(item *Item) (err error) {
-	const Tafkal = "Backstage passes to a TAFKAL80ETC concert"
-	const Sulfuras = "Sulfuras, Hand of Ragnaros"
-	const Brie = "Aged Brie"
-	if item.Name == Tafkal {
-		return UpdateTafkal(item)
-	}
-	if item.Name == Brie {
-		return UpdateBrie(item)
-	}
-	if item.Name == Sulfuras {
-		return UpdateSulf(item)
-	}
-	return UpdateOther(item)
+func UpdateQualityPerItem(item *Item) {
+	getUpdater(item).Update()
 }
--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -10,11 +10,63 @@ func TestUpdateQuality(t *testing.T) {
 		name string
 		args args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "handle empty items",
+			args: args{items: []*Item{}},
+		},
+		{
+			name: "handle Brie",
+			args: args{
+				items: []*Item{
+					{Quality: 1, Name: "Brie", SellIn: 1}, {Quality: 2, Name: "Brie", SellIn: 0},
+				},
+			},
+		},
+		{
+			name: "handle Tafkal",
+			args: args{
+				items: []*Item{
+					{Quality: 1, Name: "Tafkal", SellIn: 1}, {Quality: 2, Name: "Tafkal", SellIn: 0},
+				},
+			},
+		},
+		{
+			name: "handle Sulf",
+			args: args{
+				items: []*Item{
+					{Quality: 1, Name: "Sulf", SellIn: 1}, {Quality: 2, Name: "Sulf", SellIn: 0},
+				},
+			},
+		},
+		{
+			name: "handle Other",
+			args: args{
+				items: []*Item{
+					{Quality: 1, Name: "Other", SellIn: 1}, {Quality: 2, Name: "Other", SellIn: 0},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			UpdateQuality(tt.args.items)
+		})
+	}
+}
+
+func TestUpdateQualityPerItem(t *testing.T) {
+	type args struct {
+		item *Item
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			UpdateQualityPerItem(tt.args.item)
 		})
 	}
 }
